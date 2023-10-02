1dcb70a7465b0d15cb72c40c1f4da657820fa822
Rodwin <johnrodwin@gmail.com>
2023-10-02 16:20:01 +0200

    Intro to access tokens

--- src/UserGateway.php
+++ src/UserGateway.php
@@ -11,12 +11,23 @@ class UserGateway
 
     public function getByAPIKey(string $key): array | false
     {
-        // We only need the API key to authenticate the request so we
+        // We only need the API key to authenticate the request, so we
         // don't use the password or username yet
         $sql = "SELECT * FROM user WHERE api_key = :api_key";
 
         $stmt = $this->conn->prepare($sql);
         $stmt->bindValue(":api_key", $key, PDO::PARAM_STR);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getByUsername(string $username): mixed
+    {
+        $sql = "SELECT * FROM user WHERE username = :username";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(":username", $username, PDO::PARAM_STR);
         $stmt->execute();
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
